4aac41f27e10a1a5a2391efc58b2cca97be39bf1
walter wick <83695885+walterwick@users.noreply.github.com>
2024-04-04 13:23:27 +0300

    .

--- api/index.py
+++ api/index.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+from instaloader import instaloader
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    # Yönlendirmeyi geçici olarak devre dışı bırakmak için yorum satırı ekleyin
+    # return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return 'About'
+    # Kullanıcıyı doğrudan profile yönlendir
+    return redirect("/profile?username=emineey41")
+
+@app.route("/profile")
+def profile():
+    username = request.args.get("username")
+
+    if username:
+        try:
+            insta = instaloader.Instaloader()
+            profile = instaloader.Profile.from_username(insta.context, username)
+
+            # Profile information dictionary
+            data = {
+                "username": profile.username,
+                "post_count": profile.mediacount,
+                "followers": profile.followers,
+                "followees": profile.followees,
+                "bio": profile.biography,
+                "profile_pic_url": profile.profile_pic_url,
+                "full_name": profile.full_name,  # Might not always be available
+            }
+            return render_template("profile.html", profile_data=data)
+        except Exception as e:
+            # Handle errors gracefully
+            return render_template("profile.html", message=f"Hata: {str(e)}")
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
