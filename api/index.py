9002f5f6a5f6b3d3a58c58221ffca6877576aaf2
walter wick <83695885+walterwick@users.noreply.github.com>
2024-04-04 14:36:51 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Yönlendirmeyi geçici olarak devre dışı bırakmak için yorum satırı ekleyin
+    # Yönlendirmeyi geçici olarak devre dışı bırakmak için yorum satırı ekleyinn
     # return render_template("index.html")
 
     # Kullanıcıyı doğrudan profile yönlendir
